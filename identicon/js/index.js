ed479eefc702d2cbad0efc6df1d305c7826d61f1
Peter Donald <peter@realityforge.org>
2018-12-14 16:03:37 +1100

    Move template into the vue instance

--- identicon/js/index.js
+++ identicon/js/index.js
@@ -1,12 +1,28 @@
 new Vue({
   el: '#root',
 
+  template: `
+  <div>
+    <h3>My Identicon Generator</h3>
+    <div>
+      Input
+      <input @input="onInput" :value="inputText"/>
+    </div>
+  
+    <div>
+      Output
+      <!-- v-html inserts bound property as raw unescaped html -->
+      <div v-html="identicon"></div>
+    </div>
+  </div>
+`,
+
   // Data with initial values
   data: {
     inputText: ''
   },
   // derived data elements
-  computed:{
+  computed: {
     identicon() {
       return jdenticon.toSvg(this.inputText, 200 /* 200 pixels */);
     }
