6da9546253117056f9f7b6ddeefec567b06e5291
Peter Donald <peter@realityforge.org>
2018-12-14 17:04:41 +1100

    Strip back project

--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+// Almost all Vue applications have a main view called Vue
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
