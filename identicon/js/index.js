75f47f0ad83adf9014937761b19afa13de947df7
Peter Donald <peter@realityforge.org>
2018-12-14 15:38:13 +1100

    Add some basic comments

--- identicon/js/index.js
+++ identicon/js/index.js
@@ -1,8 +1,11 @@
 new Vue({
   el: '#root',
+
+  // Data with initial values
   data: {
     inputText: ''
   },
+  // methods invoked form component to mutate data and perform side effects
   methods: {
     onInput(event) {
       this.inputText = event.target.value;
