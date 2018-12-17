13565a1a8f3b04618550cdc6ecf1ddd1bbba827e
Peter Donald <peter@realityforge.org>
2018-12-18 09:29:13 +1100

    Extract computed to simplify code

--- video-browser/src/components/VideoListItem.vue
+++ video-browser/src/components/VideoListItem.vue
@@ -1,9 +1,15 @@
 <template>
-  <li class="list-group-item"><img :alt="video.snippet.title" :src="video.snippet.thumbnails.default.url"/> {{video.snippet.title}}</li>
+  <li class="list-group-item"><img :alt="video.snippet.title" :src="thumbnailUrl"/> {{video.snippet.title}}</li>
 </template>
 <script>
   export default {
     name: 'VideoListItem',
-    props: ['video']
+    props: ['video'],
+    computed: {
+      // Extracting a computed simplifies the code in the template
+      thumbnailUrl() {
+        return this.video.snippet.thumbnails.default.url;
+      }
+    }
   };
 </script>
