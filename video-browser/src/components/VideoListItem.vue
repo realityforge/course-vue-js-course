69eaa9ae8f4c47e96fa3ae4885140c96b3ac0dbf
Peter Donald <peter@realityforge.org>
2018-12-18 09:33:39 +1100

    Style the video item

--- video-browser/src/components/VideoListItem.vue
+++ video-browser/src/components/VideoListItem.vue
@@ -1,6 +1,12 @@
 <template>
-  <li class="list-group-item"><img :alt="video.snippet.title" :src="thumbnailUrl"/> {{video.snippet.title}}</li>
+  <li class="list-group-item media">
+    <img :alt="video.snippet.title" :src="thumbnailUrl" class="mr-3"/>
+    <div class="media-body">
+      {{video.snippet.title}}
+    </div>
+  </li>
 </template>
+
 <script>
   export default {
     name: 'VideoListItem',
@@ -13,3 +19,14 @@
     }
   };
 </script>
+
+<style scoped>
+  li {
+    display: flex;
+    cursor: pointer;
+  }
+
+  li:hover {
+    background-color: lightgrey;
+  }
+</style>
