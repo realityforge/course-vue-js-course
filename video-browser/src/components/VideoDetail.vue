acc9d7027e4eaec596fb5253a65f908159a09be7
Peter Donald <peter@realityforge.org>
2018-12-18 10:45:14 +1100

    Embed video

--- video-browser/src/components/VideoDetail.vue
+++ video-browser/src/components/VideoDetail.vue
@@ -1,5 +1,14 @@
 <template>
   <div v-if="video">
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe
+        class="embed-responsive-item"
+        :src="videoUrl"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+      </iframe>
+    </div>
     <div class="details">
       <h4>{{video.snippet.title}}</h4>
       <p>{{video.snippet.description}}</p>
@@ -10,7 +19,12 @@
 <script>
   export default {
     name: 'VideoDetail',
-    props: ['video']
+    props: ['video'],
+    computed: {
+      videoUrl() {
+        return `https://www.youtube.com/embed/${this.video.id.videoId}`;
+      }
+    }
   };
 </script>
 
