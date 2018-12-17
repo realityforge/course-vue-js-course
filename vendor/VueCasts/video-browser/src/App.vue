d489e6afe628878e1e1c0502c7716bb3587399e7
Peter Donald <peter@realityforge.org>
2018-12-18 10:46:15 +1100

    Whitespace

--- vendor/VueCasts/video-browser/src/App.vue
+++ vendor/VueCasts/video-browser/src/App.vue
@@ -2,36 +2,36 @@
   <div class="container">
     <SearchBar @termChange="onTermChange"></SearchBar>
     <div class="row">
-      <VideoDetail :video="selectedVideo" />
+      <VideoDetail :video="selectedVideo"/>
       <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import SearchBar from './components/SearchBar';
-import VideoList from './components/VideoList';
-import VideoDetail from './components/VideoDetail';
+  import axios from 'axios';
+  import SearchBar from './components/SearchBar';
+  import VideoDetail from './components/VideoDetail';
+  import VideoList from './components/VideoList';
 
-const API_KEY = 'AIzaSyB0ToJFPdt3Dp2stb8ynXqHkNe9U0ivB0c';
+  const API_KEY = 'AIzaSyB0ToJFPdt3Dp2stb8ynXqHkNe9U0ivB0c';
 
-export default {
-  name: 'App',
-  components: {
-    SearchBar,
-    VideoList,
-    VideoDetail
-  },
-  data() {
-    return { videos: [], selectedVideo: null };
-  },
-  methods: {
-    onVideoSelect(video) {
-      this.selectedVideo = video;
+  export default {
+    name: 'App',
+    components: {
+      SearchBar,
+      VideoList,
+      VideoDetail
     },
-    onTermChange(searchTerm) {
-      axios
+    data() {
+      return { videos: [], selectedVideo: null };
+    },
+    methods: {
+      onVideoSelect(video) {
+        this.selectedVideo = video;
+      },
+      onTermChange(searchTerm) {
+        axios
         .get('https://www.googleapis.com/youtube/v3/search', {
           params: {
             key: API_KEY,
@@ -43,7 +43,7 @@ export default {
         .then(response => {
           this.videos = response.data.items;
         });
+      }
     }
-  }
-};
+  };
 </script>
