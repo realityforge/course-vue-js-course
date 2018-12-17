1750625dff3c964e6456ce3770740420c06d758b
Peter Donald <peter@realityforge.org>
2018-12-18 10:13:41 +1100

    Add selected video data item and VideoDetail component to present it

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -2,6 +2,7 @@
   <!-- Can only have one top-level element in template tag -->
   <div class="container">
     <SearchBar @searchTermChanged="onSearchTermChanged"></SearchBar>
+    <VideoDetail :video="selectedVideo"></VideoDetail>
     <VideoList :videos="videos" @videoSelect="onVideoSelect"></VideoList>
   </div>
 </template>
@@ -9,6 +10,7 @@
 <script>
   import axios from 'axios';
   import SearchBar from './components/SearchBar';
+  import VideoDetail from './components/VideoDetail';
   import VideoList from './components/VideoList';
 
   // Account created via https://console.developers.google.com
@@ -18,15 +20,16 @@
   export default {
     name: 'app',
     components: {
+      VideoDetail,
       SearchBar,
       VideoList
     },
     data() {
-      return { videos: [] };
+      return { videos: [], selectedVideo: null };
     },
     methods: {
       onVideoSelect(video) {
-        console.log(video);
+        this.selectedVideo = video;
       },
       onSearchTermChanged(searchTerm) {
         axios.get('https://www.googleapis.com/youtube/v3/search',
---
+++ video-browser/src/components/VideoDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="video">{{video.snippet.title}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'VideoDetail',
+    props: ['video']
+  };
+</script>
+
+<style>
+
+</style>
