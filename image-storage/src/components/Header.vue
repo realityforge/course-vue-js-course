93b26a2a7fe475146648f7dc1ea29d7629652eb7
Peter Donald <peter@realityforge.org>
2018-12-25 10:14:01 +1100

    Update the links

--- image-storage/src/components/Header.vue
+++ image-storage/src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="ui secondary pointing menu">
-    <a href="" class="active item">Image Storage</a>
+    <router-link to="/" class="active item">Image Storage</router-link>
     <div class="right menu">
       <a v-if="!isAuthenticated" href="#" @click="login" class="ui item">Login</a>
       <div v-if="isAuthenticated" class="horizontal">
-        <a href="" class="item">Galleries</a>
-        <a href="" class="item">Upload</a>
+        <router-link to="/" class="item">Galleries</router-link>
+        <router-link to="/upload" class="item">Upload</router-link>
         <a href="#" class="item" @click="logout">Logout</a>
       </div>
     </div>
