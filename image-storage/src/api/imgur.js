2d5cae05b63eddfb182a015717a735596102318c
Peter Donald <peter@realityforge.org>
2018-12-24 22:48:00 +1100

    Comment out to avoid code warning

--- image-storage/src/api/imgur.js
+++ image-storage/src/api/imgur.js
@@ -1,7 +1,7 @@
 import qs from 'qs';
 
 const IMGUR_CLIENT_ID = '11821b8778e97c4';
-const IMGUR_CLIENT_SECRET = 'a51d4a83c97739f074196b60863ff00d71d9eb69';
+//const IMGUR_CLIENT_SECRET = 'a51d4a83c97739f074196b60863ff00d71d9eb69';
 
 const ROOT_URL = `https://api.imgur.com`;
 
