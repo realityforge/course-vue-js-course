72dce257331e8292c0851ecd98d7177a5405fb3e
Peter Donald <peter@realityforge.org>
2018-12-24 22:46:44 +1100

    Initial routing integration

--- image-storage/src/main.js
+++ image-storage/src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 import store from './store';
 
 Vue.config.productionTip = false;
 
+// Enable router plugin
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  // This history is browser based history rather than hash based
+  // this is needed to enable oauth integration
+  mode: 'history',
+  routes: [
+  ]
+});
+
 new Vue({
   render: h => h(App),
+  router,
   store
 }).$mount('#app');
