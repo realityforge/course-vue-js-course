bfbbc193a7a14c6fc2d0338d4a42ddadee21157a
Peter Donald <peter@realityforge.org>
2018-12-26 08:59:00 +1100

    Add input control and style it reasonably

--- image-storage/src/components/ImageUpload.vue
+++ image-storage/src/components/ImageUpload.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>ImageUpload</div>
+  <div class="dropper">
+    <input type="file">
+    <span>Drop file here.</span>
+  </div>
 </template>
 
 <script>
@@ -7,3 +10,31 @@
     name: 'ImageUpload'
   };
 </script>
+
+<style scoped>
+  .dropper {
+    height: 30vh;
+    border: 2px dashed black;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+
+  .dropper:hover {
+    background-color: #eee;
+  }
+
+  input {
+    width: 100%;
+    height: 30vh;
+    /*
+    the input fills the entire container due to absolute positioning
+    and the container is the parent relative
+    */
+    position: absolute;
+    /* invisible! but can still interact with it. */
+    opacity: 0;
+  }
+</style>
